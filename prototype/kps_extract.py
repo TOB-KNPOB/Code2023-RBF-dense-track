4fd6dbb478e56480270b6399018ccc0605106dfe
KNPOB <qilong-kirov.liu@outlook.com>
2023-11-07 16:47:41 +0800

    spactial alignment of 3dmd & vicon
    based on a static posture dual-recording

---
+++ prototype/kps_extract.py
@@ -0,0 +1,9 @@
+from mesh4d.analyse.crave import obj_pick_points
+
+obj_pick_points(
+    filedir='/Users/knpob/Territory/2-Kolmo/4-Dataset/20211229-Running breast scanning/3dmd/0kmh_8marker_bra/static_standing_with_bra.000001.obj',
+    use_texture=True,
+    is_save=True,
+    save_folder='prototype/output',
+    save_name='points_3dmd',
+)
